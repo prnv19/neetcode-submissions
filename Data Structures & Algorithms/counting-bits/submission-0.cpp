9e76c888351860e696c8dd01bd68e2870438034d
Pranav <78401509+prnv19@users.noreply.github.com>
2026-08-16 16:21:46 -0700

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> dp(n + 1, 0);
+        int offset = 1;
+
+        for (int i = 1 ; i < n + 1; i++){
+            if (i == 2 * offset) offset = i;
+            dp[i] = 1 + dp[i - offset];
+        }
+        return dp;
+    }
+};
